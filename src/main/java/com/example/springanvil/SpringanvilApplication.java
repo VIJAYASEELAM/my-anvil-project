1e450662a0bde8a50887aeb6bc0f146b1178dace
VIJAYASEELAM <vijayaseelam2004@gmail.com>
2026-02-21 18:32:32 +0000

    Initial Spring Boot setup

---
+++ src/main/java/com/example/springanvil/SpringanvilApplication.java
@@ -0,0 +1,13 @@
+package com.example.springanvil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringanvilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringanvilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springanvil/SpringanvilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springanvil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringanvilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
